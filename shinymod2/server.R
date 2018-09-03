42eb4d91aa9245d0cf42bf065aa38884da90d920
Martin Tobon <martin.tobon@gmail.com>
2018-09-03 16:15:42 -0700

    Working!!

--- shinymod2/server.R
+++ shinymod2/server.R
@@ -1,7 +1,9 @@
-
-
-
+library(shiny)
+library(DMwR2)
+library(ggplot2)
+library(xts)
 library(quantmod)
+library(forecast)
 
 
 # Define server logic required to draw a histogram
@@ -11,19 +13,108 @@ shinyServer(function(input, output) {
     
     dataInput <- reactive({
       getSymbols(input$stockInput, src = "yahoo",
-                 from='2017-01-01', 
-                 to='2017-04-30',
+                 from='2018-01-01', 
+                 to='2018-08-30',
                  auto.assign = FALSE)
     })
     
     output$plot <- renderPlot({    
       chartSeries(dataInput(), theme = chartTheme("white"),
                   type = "line")
+    })
+    
+    output$autoarima <- renderPlot({
+      
+      
+      
+      # retrieve stock data and produce ARIMA non-seasonal model estimate
+      stock<-as.character(input$stockInput)
+      stock_df<-as.data.frame(getSymbols((Symbols=stock), auto.assign=FALSE, from='2018-01-01'))
+      stock_df$Open=stock_df[,1]
+      stock_df$High=stock_df[,2]
+      stock_df$Low=stock_df[,3]
+      stock_df$Close=stock_df[,4]
+      stock_df$Volume=stock_df[,5]
+      stock_df$Adj=stock_df[,6]
+      stock_df<-stock_df[,c(7,8,9,10,11,12)]
+      
+      model1<-auto.arima(stock_df$Close, ic="bic")
+      forecast1<-forecast(model1, h=5)
+      
+      # draw the line graph
+      plot(forecast1)
+      
+      if (input$modelInput=="ARIMA Non-seasonal"){
+        return(plot(forecast1))
+      } else {
+        return(NULL)
+      }
+      
+      
       
     })
     
+    #ARIMA Seasonal
+    output$arimaseasonal<-renderPlot({
+      
+      # retrieve stock data
+      stock<-as.character(input$stockInput)
+      stock_df<-as.data.frame(getSymbols((Symbols=stock), auto.assign=FALSE, from='2018-01-01'))
+      stock_df$Open=stock_df[,1]
+      stock_df$High=stock_df[,2]
+      stock_df$Low=stock_df[,3]
+      stock_df$Close=stock_df[,4]
+      stock_df$Volume=stock_df[,5]
+      stock_df$Adj=stock_df[,6]
+      stock_df<-stock_df[,c(7,8,9,10,11,12)]
+      
+      #Prepare data for ARIMA seasonal model
+      stock_df$v7_MA<-ma(stock_df$Close, order=7)
+      station<-ts(na.omit(stock_df$v7_MA), frequency=30)
+      decomp<-stl(station, s.window="periodic")
+      adjusted<-seasadj(decomp)
+      
+      #Fit ARIMA seasonal model
+      model2<-auto.arima(adjusted, seasonal=TRUE)
+      forecast2<-forecast(model2, h=5)
+      
+      if (input$modelInput=="ARIMA Seasonal"){
+        return(plot(forecast2))
+      } else {
+        return(NULL)
+      }
+    })
+    
+    #Exponential Smoothing
+    output$ets<-renderPlot({
+      
+      # retrieve stock data
+      stock<-as.character(input$stockInput)
+      stock_df<-as.data.frame(getSymbols((Symbols=stock), auto.assign=FALSE, from='2018-01-01'))
+      stock_df$Open=stock_df[,1]
+      stock_df$High=stock_df[,2]
+      stock_df$Low=stock_df[,3]
+      stock_df$Close=stock_df[,4]
+      stock_df$Volume=stock_df[,5]
+      stock_df$Adj=stock_df[,6]
+      stock_df<-stock_df[,c(7,8,9,10,11,12)]
+      
+      #Fit Exponential Smoothing model
+      model3<-ets(stock_df$Close)
+      forecast3<-forecast(model3, h=5)
+      
+      if (input$modelInput=="Exponential Smoothing (ETS)"){
+        return(plot(forecast3))
+      } else {
+        return(NULL)
+      }
+      
+      
+    })
+    
+    
+    
   })
   
-
   
 })
